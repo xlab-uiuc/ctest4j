60a3ac27d739d6abee1c41587ecc55c57d2965bd
shuaiwang516 <shuaiwang516@gmail.com>
2023-11-02 16:24:22 -0500

    change one normal test

--- ctest-runner-junit5/src/test/java/edu/illinois/junit5/NormalTest.java
+++ ctest-runner-junit5/src/test/java/edu/illinois/junit5/NormalTest.java
@@ -1,23 +1,25 @@
 package edu.illinois.junit5;
 
-import edu.illinois.CTest;
+import edu.illinois.CTestJunit5Extension;
 import edu.illinois.Configuration;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 /**
  * Author: Shuai Wang
  * Date:  11/1/23
  */
+@ExtendWith(CTestJunit5Extension.class)
 public class NormalTest {
-    @CTest
+    @Test
     public void test() {
         Configuration conf = new Configuration();
-        conf.get("tracked-parameter");
+        conf.get("tracked-parameter1");
     }
 
     @Test
     public void test2() {
         Configuration conf = new Configuration();
-        conf.get("tracked-parameter");
+        conf.get("tracked-parameter2");
     }
 }

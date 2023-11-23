eebc3b3d12a6c0542ebd99c75209df97a698e8ed
“Xinyu <xinyulianuiuc@gmail.com>
2023-11-22 19:35:17 -0600

    nits

--- ctest-runner-common/src/main/java/edu/illinois/agent/AspectJWeaver.java
+++ ctest-runner-common/src/main/java/edu/illinois/agent/AspectJWeaver.java
@@ -1,12 +1,8 @@
 package edu.illinois.agent;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -17,35 +13,35 @@ import static edu.illinois.Names.*;
 @Aspect
 public class AspectJWeaver {
 
-    static String[] config_getter_list;
-    static String[] config_setter_list;
+    private static String[] config_getter_list;
+    private static String[] config_setter_list;
+
     static {
         String config_getter_str = System.getProperty(CTEST_GETTER);
         String config_setter_str = System.getProperty(CTEST_SETTER);
-        if (config_getter_str != null) {
-            config_getter_list = config_getter_str.split(",");
-        }
-        if (config_setter_str != null) {
-            config_setter_list = config_setter_str.split(",");
-        }
+        config_getter_list = config_getter_str != null ? config_getter_str.split(",") : new String[0];
+        config_setter_list = config_setter_str != null ? config_setter_str.split(",") : new String[0];
     }
 
     @Pointcut("execution(public * *(String)) && args(name)")
-    public void anyMethod(String name) {}
+    public void anyGetMethod(String name) {}
 
-    @Before("anyMethod(name)")
-    public void beforeAnyMethod(JoinPoint joinPoint, String name) {
+    @Before("anyGetMethod(name)")
+    public void beforeAnyGetMethod(JoinPoint joinPoint, String name) {
         String methodSignature = joinPoint.getSignature().toString();
-        if (config_getter_list != null) {
-            if (Arrays.asList(config_getter_list).contains(methodSignature)) {
-                ConfigTracker.markParamAsUsed(name);
-            }
-        }
-        if (config_setter_list != null) {
-            if (Arrays.asList(config_setter_list).contains(methodSignature)) {
-                ConfigTracker.markParmaAsSet(name);
-            }
+        if (Arrays.asList(config_getter_list).contains(methodSignature)) {
+            ConfigTracker.markParamAsUsed(name);
         }
     }
 
+    @Pointcut("execution(public * *(String, String)) && args(name, value)")
+    public void anySetMethod(String name, String value) {}
+
+    @Before("anySetMethod(name, value)")
+    public void beforeAnySetMethod(JoinPoint joinPoint, String name, String value) {
+        String methodSignature = joinPoint.getSignature().toString();
+        if (Arrays.asList(config_setter_list).contains(methodSignature)) {
+            ConfigTracker.markParmaAsSet(name);
+        }
+    }
 }

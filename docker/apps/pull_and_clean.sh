61c464dad774c1cbae9f902651adc6c3c83dbab1
shuaiwang516 <shuaiwang516@gmail.com>
2023-12-06 19:52:13 -0600

    build ctest-runner with main branch

--- docker/apps/pull_and_clean.sh
+++ docker/apps/pull_and_clean.sh
@@ -4,9 +4,9 @@ for f in $(find . -maxdepth 1 -mindepth 1 -type d);
 do
     # if f contains "ctest-runner" then skip
     if [[ $f == *"ctest-runner"* ]]; then
-	(cd $f && git restore . && git status  && git checkout junit5  && git pull && mvn clean)
+	(cd $f && git restore . && git status  && git checkout main && git pull && mvn clean)
     else
 	echo $f
-	(cd $f && git restore . && git status  && git checkout ctest-eval  && git pull && mvn clean)
+	(cd $f && git restore . && git status  && git checkout ctest-eval && git pull && mvn clean)
     fi
 done

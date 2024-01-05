4381ac64d1fb556ec4204d45658391d54b7c4fe3
shuaiwang516 <shuaiwang516@gmail.com>
2024-01-05 14:06:22 -0800

    test the new version injection

---
+++ ctest-runner-junit4/src/test/java/edu/illinois/junit4/designB/TestInjection.java
@@ -0,0 +1,40 @@
+package edu.illinois.junit4.designB;
+
+import edu.illinois.CTest;
+import edu.illinois.CTestClass;
+import edu.illinois.CTestJUnit4Runner2;
+import edu.illinois.Configuration;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+/**
+ * Author: Shuai Wang
+ * Date:  11/4/23
+ */
+@RunWith(CTestJUnit4Runner2.class)
+@CTestClass
+public class TestInjection {
+    Configuration conf = null;
+    @BeforeClass
+    public static void setUp() {
+        Configuration conf = new Configuration();
+        conf.get("beforeClass");
+    }
+
+    @Before
+    public void setUp2() {
+        conf = new Configuration();
+        conf.get("before");
+    }
+
+    @CTest(value = {"beforeClass", "before", "param1"})
+    public void test() {
+        conf.get("param1");
+    }
+
+    @CTest(value = {"beforeClass", "before", "param2"})
+    public void test2() {
+        conf.get("param2");
+    }
+}
